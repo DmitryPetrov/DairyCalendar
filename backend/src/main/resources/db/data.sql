7cbe59736da2300d59e4fc5f38c5c256eac00551
dmitrypetrov <dmitrypetrov93@gmail.com>
2025-03-19 11:01:20 +0300

    fix: sql script

--- backend/src/main/resources/db/data.sql
+++ backend/src/main/resources/db/data.sql
@@ -2,8 +2,8 @@
 INSERT INTO public."app-user" (created_at, updated_at, username, password, role_id) VALUES ('2023-02-23', '2023-02-23', 'user_1', '$2a$10$.Av4V/gzwRX7GLSzmM0LC.pfwYxCrFyfme6CgJJG8FTI6yBqczZp6', 2);
 INSERT INTO public."app-user" (created_at, updated_at, username, password, role_id) VALUES ('2023-02-23', '2023-02-23', 'user_2', '$2a$10$xep/PsfIZYsMPxbqbQBBouSNG5AyTohGAj6wYg9zDCXFYBl/2Zf0q', 2);
 
-INSERT INTO "public"."tag" ("created_at", "updated_at", "tag") VALUES ('2023-02-23 12:45:52.151458', '2023-02-23 12:45:52.151467', 'tag_1');
-INSERT INTO "public"."tag" ("created_at", "updated_at", "tag") VALUES ('2023-02-23 12:45:52.150940', '2023-02-23 12:45:52.150960', 'tag_2');
-INSERT INTO "public"."tag" ("created_at", "updated_at", "tag") VALUES ('2023-02-23 12:46:27.386056', '2023-02-23 12:46:27.386074', 'tag_3');
-INSERT INTO "public"."tag" ("created_at", "updated_at", "tag") VALUES ('2023-02-23 12:46:27.386056', '2023-02-23 12:46:27.386074', 'tag_4');
-INSERT INTO "public"."tag" ("created_at", "updated_at", "tag") VALUES ('2023-02-23 12:46:27.386056', '2023-02-23 12:46:27.386074', 'tag_5');
+INSERT INTO public.tag (created_at, updated_at, tag) VALUES ('2023-02-23 12:45:52.151458', '2023-02-23 12:45:52.151467', 'tag_1');
+INSERT INTO public.tag (created_at, updated_at, tag) VALUES ('2023-02-23 12:45:52.150940', '2023-02-23 12:45:52.150960', 'tag_2');
+INSERT INTO public.tag (created_at, updated_at, tag) VALUES ('2023-02-23 12:46:27.386056', '2023-02-23 12:46:27.386074', 'tag_3');
+INSERT INTO public.tag (created_at, updated_at, tag) VALUES ('2023-02-23 12:46:27.386056', '2023-02-23 12:46:27.386074', 'tag_4');
+INSERT INTO public.tag (created_at, updated_at, tag) VALUES ('2023-02-23 12:46:27.386056', '2023-02-23 12:46:27.386074', 'tag_5');
