a439c5ed4032b900f8fbc79f0c4dcd5b6a2eb7e5
dmitrypetrov <dmitrypetrov93@gmail.com>
2025-03-19 11:55:29 +0300

    update: back cors setting for dev profile

--- backend/src/main/java/net/personal/dairycalendar/config/SecurityConfig.java
+++ backend/src/main/java/net/personal/dairycalendar/config/SecurityConfig.java
@@ -14,7 +14,6 @@ import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 import java.util.List;
 
@@ -65,9 +64,10 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of(CorsConfiguration.ALL));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
         configuration.setAllowedMethods(List.of(CorsConfiguration.ALL));
         configuration.setAllowedHeaders(List.of(CorsConfiguration.ALL));
+        configuration.setAllowCredentials(true);
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
